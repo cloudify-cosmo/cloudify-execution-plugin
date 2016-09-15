080a8e1b338334bbab546022c0620b084f0d1f26
fogelomer <omer@gigaspaces.com>
2016-09-15 15:14:35 +0300

    test

--- execution_plugin/winrm_plugin/tests/unit_test_winrm.py
+++ execution_plugin/winrm_plugin/tests/unit_test_winrm.py
@@ -24,8 +24,8 @@ class TestWinrmPlugin(unittest.TestCase):
 
     @patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
     def test_01_check_remote_path(self):
-        password = os.environ['service_password']
-        user = os.environ['service_username']
+        password = "uDy=rr@Jm%9c~pe"
+        user = "appveyor"
         s = winrm.Session('localhost', auth=(user, password))
         r = s.run_cmd('ipconfig', ['/all'])
         print(r.status_code, r.std_out)
