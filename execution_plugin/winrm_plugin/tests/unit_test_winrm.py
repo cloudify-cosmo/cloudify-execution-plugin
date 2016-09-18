3648663dc40e7fa2f4924d0311069243737bfd8d
fogelomer <omer@gigaspaces.com>
2016-09-18 12:16:58 +0300

    test

--- execution_plugin/winrm_plugin/tests/unit_test_winrm.py
+++ execution_plugin/winrm_plugin/tests/unit_test_winrm.py
@@ -22,7 +22,7 @@ class TestWinrmPlugin(unittest.TestCase):
     def tearDown(self):
         pass
 
-    @pytest.fixture(scope='function')
+    @pytest.fixture(scope='module')
     @patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
     def test_01_check_remote_path(self):
         path = tempfile.gettempdir()
