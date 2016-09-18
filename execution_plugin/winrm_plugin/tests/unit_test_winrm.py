951ae2e6017fb2fd171d29592957213f76bb7d4e
fogelomer <omer@gigaspaces.com>
2016-09-18 17:34:32 +0300

    test

--- execution_plugin/winrm_plugin/tests/unit_test_winrm.py
+++ execution_plugin/winrm_plugin/tests/unit_test_winrm.py
@@ -13,7 +13,33 @@ from cloudify.mocks import MockCloudifyContext
 
 from .. import tasks
 
-@pytest.mark.usefixtures("protocol_fake")
+
+
+@pytest.fixture(scope='module')
+def protocol_fake(request):
+    uuid4_patcher = patch('uuid.uuid4')
+    uuid4_mock = uuid4_patcher.start()
+    uuid4_mock.return_value = uuid.UUID(
+        '11111111-1111-1111-1111-111111111111')
+
+    from winrm.protocol import Protocol
+
+    protocol_fake = Protocol(
+        endpoint='http://windows-host:5985/wsman',
+        transport='plaintext',
+        username='john.smith',
+        password='secret')
+
+    protocol_fake.transport = TransportStub()
+
+    def uuid4_patch_stop():
+        uuid4_patcher.stop()
+
+    request.addfinalizer(uuid4_patch_stop)
+    return protocol_fake
+
+
+
 @patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
 def test_01_check_remote_path(protocol_fake):
     path = tempfile.gettempdir()
