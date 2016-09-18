eedb900e403aeb00fe91502ba220923fc9be3e6a
fogelomer <omer@gigaspaces.com>
2016-09-18 12:20:22 +0300

    test

--- execution_plugin/winrm_plugin/tests/unit_test_winrm.py
+++ execution_plugin/winrm_plugin/tests/unit_test_winrm.py
@@ -22,11 +22,10 @@ class TestWinrmPlugin(unittest.TestCase):
     def tearDown(self):
         pass
 
-    @pytest.fixture(scope='module')
     @patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
-    def test_01_check_remote_path(self):
+    def test_01_check_remote_path(self, protocol_fake):
         path = tempfile.gettempdir()
-        id = conftest.protocol_fake.open_shell()
+        id = protocol_fake.open_shell()
         self.assertTrue(tasks.check_remote_path(id, path))
         path = 'non-exists'
         self.assertFalse(tasks.check_remote_path(id, path))
