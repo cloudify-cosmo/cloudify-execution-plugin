12b71f24c39bae880dd6b064073e1e81e129721c
fogelomer <omer@gigaspaces.com>
2016-09-19 17:10:37 +0300

    test

--- execution_plugin/winrm_plugin/tests/test_winrm.py
+++ execution_plugin/winrm_plugin/tests/test_winrm.py
@@ -6,8 +6,8 @@ import shutil
 from idlelib import PyShell
 import unittest
 import tempfile
-from winrm.tests.conftest import protocol_fake
-from winrm.tests.conftest import protocol_real
+# from winrm.tests.conftest import protocol_fake
+# from winrm.tests.conftest import protocol_real
 from mock import patch
 import pytest
 from cloudify.mocks import MockCloudifyContext
@@ -15,16 +15,16 @@ from cloudify.mocks import MockCloudifyContext
 from .. import tasks
 
 @patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
-def test_01_check_remote_path():
+def test_01_check_remote_path(protocol_fake):
     path = tempfile.gettempdir()
     id = protocol_fake.open_shell()
     assert tasks.check_remote_path(id, path, protocol_fake) == True
     path = 'non-exists'
     assert tasks.check_remote_path(id, path, protocol_fake) == False
 
-def test_02_check_remote_path():
+def test_02_check_remote_path(protocol_real):
     path = tempfile.gettempdir()
     id = protocol_real.open_shell()
-    assert tasks.check_remote_path(id, path, protocol_fake) == True
+    assert tasks.check_remote_path(id, path, protocol_real) == True
     path = 'non-exists'
-    assert tasks.check_remote_path(id, path, protocol_fake) == False
+    assert tasks.check_remote_path(id, path, protocol_real) == False
