d651fb308f8a9c04d79f8c937c1c0187db41a094
fogelomer <omer@gigaspaces.com>
2016-09-19 17:47:15 +0300

    test

--- execution_plugin/winrm_plugin/tests/test_winrm.py
+++ execution_plugin/winrm_plugin/tests/test_winrm.py
@@ -14,13 +14,13 @@ from cloudify.mocks import MockCloudifyContext
 
 from .. import tasks
 
-# @patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
-# def test_01_check_remote_path(protocol_fake):
-#     path = tempfile.gettempdir()
-#     id = protocol_fake.open_shell()
-#     assert tasks.check_remote_path(id, path, protocol_fake) == True
-#     path = 'non-exists'
-#     assert tasks.check_remote_path(id, path, protocol_fake) == False
+@patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
+def test_01_check_remote_path(protocol_fake):
+    path = tempfile.gettempdir()
+    id = protocol_fake.open_shell()
+    assert tasks.check_remote_path(id, path, protocol_fake) == True
+    path = 'non-exists'
+    assert tasks.check_remote_path(id, path, protocol_fake) == False
 
 @patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
 def test_02_check_remote_path(protocol_real):
