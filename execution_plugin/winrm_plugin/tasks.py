4451a92d54bb58f8ef8e0655dc58961bc70ad436
fogelomer <omer@gigaspaces.com>
2016-09-19 17:40:31 +0300

    test

--- execution_plugin/winrm_plugin/tasks.py
+++ execution_plugin/winrm_plugin/tasks.py
@@ -207,8 +207,9 @@ def check_remote_path(remote_shell_id, cmd_path, conn):
     '''
     verifying user path exists.
     '''
+    cmd = create_encoded_command(cmd_path)
     try:
-        command_id = conn.run_command(remote_shell_id, 'TEST-PATH', cmd_path)
+        command_id = conn.run_command(remote_shell_id, 'TEST-PATH', cmd)
         stdout, stderr, return_code = conn.get_command_output(remote_shell_id,
                                                               command_id)
         conn.cleanup_command(remote_shell_id, command_id)
