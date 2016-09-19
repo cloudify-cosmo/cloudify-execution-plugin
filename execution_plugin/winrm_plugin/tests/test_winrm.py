6cef543ac5e9cce9b55d31848477db4c6f4f129c
fogelomer <omer@gigaspaces.com>
2016-09-19 16:57:35 +0300

    test

--- execution_plugin/winrm_plugin/tests/test_winrm.py
+++ execution_plugin/winrm_plugin/tests/test_winrm.py
@@ -6,7 +6,6 @@ import shutil
 from idlelib import PyShell
 import unittest
 import tempfile
-from winrm.tests.conftest import protocol_fake
 from mock import patch
 import pytest
 from cloudify.mocks import MockCloudifyContext
