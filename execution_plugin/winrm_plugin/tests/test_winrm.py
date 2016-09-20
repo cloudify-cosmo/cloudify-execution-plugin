a6b959586f823adee251ba3cfd093e41ade98326
fogelomer <omer@gigaspaces.com>
2016-09-20 11:32:10 +0300

    test

--- execution_plugin/winrm_plugin/tests/test_winrm.py
+++ execution_plugin/winrm_plugin/tests/test_winrm.py
@@ -22,3 +22,10 @@ def test_01_check_remote_path(protocol_real):
     assert tasks.check_remote_path(shell_id, path, protocol_real)
     path = 'non-exists'
     assert not tasks.check_remote_path(shell_id, path, protocol_real)
+
+@patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
+def test_02_run_remote_command(protocol_real):
+    path = tempfile.gettempdir()
+    shell_id = protocol_real.open_shell()
+    tasks.run_remote_command(shell_id, 'powershell', path, 'echo 1', protocol_real)
+    
