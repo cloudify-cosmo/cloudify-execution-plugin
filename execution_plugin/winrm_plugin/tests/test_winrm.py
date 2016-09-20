32d3f9011bc58f7e34c50700302fbf98888fdaff
fogelomer <omer@gigaspaces.com>
2016-09-20 13:43:50 +0300

    test

--- execution_plugin/winrm_plugin/tests/test_winrm.py
+++ execution_plugin/winrm_plugin/tests/test_winrm.py
@@ -6,7 +6,7 @@ import shutil
 from idlelib import PyShell
 import unittest
 import tempfile
-from winrm.tests import conftest
+from winrm.tests.conftest import protocol_fake, protocol_real
 
 from mock import patch
 import pytest
@@ -14,13 +14,13 @@ from cloudify.mocks import MockCloudifyContext
 
 from .. import tasks
 
-pytestmark = pytest.mark.usefixtures("protocol_real", "protocol_fake")
+pytestmark = pytest.mark.usefixtures("protocol_real")
 
 
 @patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
 def test_01_check_remote_path(pytestmark):
     path = tempfile.gettempdir()
-    shell_id = protocol_real.open_shell()
+    shell_id = pytestmark.open_shell()
     assert tasks.check_remote_path(shell_id, path, protocol_real)
     path = 'non-exists'
     assert not tasks.check_remote_path(shell_id, path, protocol_real)
@@ -29,5 +29,5 @@ def test_01_check_remote_path(pytestmark):
 @patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
 def test_02_run_remote_command(pytestmark):
     path = tempfile.gettempdir()
-    shell_id = protocol_real.open_shell()
+    shell_id = pytestmark.open_shell()
     tasks.run_remote_command(shell_id, 'powershell', path, 'echo 1', protocol_real)
