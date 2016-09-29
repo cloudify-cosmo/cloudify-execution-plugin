d31dd3472287a1fea6da27b2131ab2e740dd3711
fogelomer <omer@gigaspaces.com>
2016-09-29 11:50:55 +0300

    test

--- execution_plugin/winrm_plugin/tests/test_winrm.py
+++ execution_plugin/winrm_plugin/tests/test_winrm.py
@@ -23,20 +23,20 @@ for elem in list:
         print(b)
     os.environ[elem] = b
 
-print(os.environ['WINRM_USERNAME'])
-print(os.environ['WINRM_PASSWORD'])
-print(os.environ['WINRM_TRANSPORT'])
-print(os.environ['WINRM_ENDPOINT'])
+
+@patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
+def test_01_check_remote_path(protocol_fake):
+    print(os.environ['WINRM_USERNAME'])
+    print(os.environ['WINRM_PASSWORD'])
+    print(os.environ['WINRM_TRANSPORT'])
+    print(os.environ['WINRM_ENDPOINT'])
+    path = tempfile.gettempdir()
+    shell_id = protocol_fake.open_shell()
+    assert tasks.check_remote_path(shell_id, path, protocol_fake)
+    path = 'non-exists'
+    assert not tasks.check_remote_path(shell_id, path, protocol_fake)
 #
-# @patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
-# def test_01_check_remote_path(protocol_fake):
-#     path = tempfile.gettempdir()
-#     shell_id = protocol_fake.open_shell()
-#     assert tasks.check_remote_path(shell_id, path, protocol_fake)
-#     path = 'non-exists'
-#     assert not tasks.check_remote_path(shell_id, path, protocol_fake)
 #
-# 
 # @patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
 # def test_02_run_remote_command(protocol_real):
 #     path = tempfile.gettempdir()
