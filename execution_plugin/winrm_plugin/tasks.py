9b7ae1e92ccc55400158a2838053bdb6f20af9d9
fogelomer <omer@gigaspaces.com>
2016-10-18 10:57:24 +0300

    tests

--- execution_plugin/winrm_plugin/tasks.py
+++ execution_plugin/winrm_plugin/tasks.py
@@ -100,7 +100,7 @@ def get_remote_shell_id(winrm_protocol):
     if winrm env was initialized successfully.
     '''
     try:
-        print winrm_protocol.transport, winrm_protocol.endpoint
+        print (winrm_protocol.username, winrm_protocol.endpoint)
         return winrm_protocol.open_shell()
     except (AttributeError,
             exceptions.WinRMWebServiceError,
