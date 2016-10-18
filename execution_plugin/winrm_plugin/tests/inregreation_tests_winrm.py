f7a266306bd9d46b4974bf48450969b074505cb1
fogelomer <omer@gigaspaces.com>
2016-10-18 12:17:11 +0300

    tests

--- execution_plugin/winrm_plugin/tests/inregreation_tests_winrm.py
+++ execution_plugin/winrm_plugin/tests/inregreation_tests_winrm.py
@@ -31,9 +31,9 @@ def test_01_run_remote_command(protocol_real):
     protocol_real.close_shell(shell_id)
 
 @patch('execution_plugin.winrm_plugin.tasks.ctx', MockCloudifyContext())
-def test_02_run_script(protocol_real, mocker):
+def test_02_run_script(protocol_real):
     x = protocol_real
-    mocker.patch('execution_plugin.winrm_plugin.tasks.get_winrm_protocol', return_value=x)
+    patch('execution_plugin.winrm_plugin.tasks.get_winrm_protocol', return_value=x)
     tasks.run_script('http://localhost:5985/wsman', 'Administrator', 'Aa123456', 'powershell', os.path.join('scripts', 'test.ps1'))
     tasks.run_script('http://localhost:5985/wsman', 'Administrator', 'Aa123456', 'cmd', os.path.join('scripts', 'test.bat'))
 #
